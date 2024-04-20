e859f9c4c170e9d027b6b8385be9db0511a99d23
Eliseu dos Santos da Silva <s.eliseu.santos@gmail.com>
2024-04-19 23:11:16 -0300

    first commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-    apps: [
-        {
-            "name": "agency-pro",
-            "script": ".output/server/index.mjs",
-            "node_args": ["--experimental-modules"],
-            "env": {
-                "NODE_ENV": "production"
-            }
-        }
-    ]
-};
+  apps: [
+    {
+      name: 'agencyPRO',
+      port: '3987',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
